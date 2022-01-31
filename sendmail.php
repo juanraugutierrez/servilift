48ac010a3a42766baf9f5b9ea4cab72d4ac1b8b4
Juan Gutiérrez Calquín <juan.gutierrezcalquin@gmail.com>
2022-01-31 16:10:49 -0300

    primer uptime

---
+++ sendmail.php
@@ -0,0 +1,369 @@
+﻿<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="es"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="es"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta name="robots" content="follow,index" />
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, mi
+  <link rel="canonical" href="/es/" />
+
+  <title>Bienvenido a Servilift | Contacto</title>
+
+  <!-- STYLESHEETS -->
+  <link rel="stylesheet" type="text/css" href="css/screen.css" />   
+   <link rel="stylesheet" type="text/css" href="css/main.css" />  
+  <link rel="stylesheet" type="text/css" media="print" href="css/print.css" />
+  <link rel="stylesheet" type="text/css"  href="bootstrap/css/bootstrap.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+
+
+  <script type="text/javascript" src="bootstrap/js/Bootstrap.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+   <style>
+   .carousel {
+    position: relative;
+    width-max: inherit;
+    padding-left: 30px;
+}
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 550px;
+      height: 245px;
+      margin: auto;
+  }
+  </style>
+  <body>
+    <header>
+  <div class="row">
+  <div class="container-fluid">    
+
+  <div class="left">
+  
+  <a title="Home" href="/" target="_self">
+  
+    <img class="left show1 logo" src="img/logo.png"  alt="" />
+    <img class="left show2 logo" src="img/logo.png"  alt="" />
+    <img class="left show3 logo" src="img/logo.png"  alt="" />
+    <img class="left show4 logo" src="img/logo.png"  alt="" />
+    <img class="left show5 logo" src="img/logo.png"  alt="" />
+  
+  </a>
+  
+</div>
+<!--<div class="portal">
+  <a title="Portal de Clientes" href="#">Portal de Clientes »</a>
+</div>-->
+<div class="search">
+  <a title="Google_Site_Search_Header Search" class="magnifier" href="#" target="_self">Google_Site_Search_Header Search</a>
+  
+  <!--<form method="GET" class="bhvSearchBox" action="/es/busqueda-resultados.aspx">
+    
+    <input type="text" name="search" id="search" class="search" />
+    <input type="submit" value="" id="search-submit" class="submitBtn button postfix" />
+
+  </form> -->
+  
+</div>
+<div class="links">
+  
+  
+  <!--<a title="Trabaja en Servilift" href="#">Trabaja en Servilift &raquo;</a>-->
+  
+</div><div class="language-links"> 
+  
+  
+ 
+  
+</div>     
+  </div>
+  </div>
+</header>
+<div class="row">    
+  <div class="ten columns">
+    <nav class="top-bar">
+      <ul>
+      <li class="section-path">
+        <a href="/es/" class="home"></a> 
+      
+      </li>
+      <li class="toggle-topbar">
+      <a href="#"></a><div class="toggle-nav">Navegación</div>        
+      </li>
+      </ul>
+      <section>
+             
+          <ul>
+            <li><a class="selected" href="index.htm">Home</a></li>
+            <li><a href="productosyservicios.htm">Productos y Servicios</a></li>
+            <li  class="active"><a href="mantenimiento.htm">Mantenimiento</a></li>
+            <li><a href="#">Acceso a Clientes</a></li>
+            <li><a href="acercadeservilift.htm">Acerca de Servilift</a></li>
+            <li><a href="contacto.htm">Contacto</a></li>
+          </ul>
+
+          
+        
+        
+      </section>
+    </nav>
+  </div>
+</div> 
+
+  <section>
+  <div class="row">
+<div class="col-sm-8">
+  <div class="containerp block slider-container"">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+        <img src="img/ascensor1.jpg" alt="Chania" width="500" height="245">
+        <div class="carousel-caption">
+          <h3>Asesorias</h3>
+          <p>Asesorias que lo apoya desde la adquisicion del ascensor hasta el diseño del plan de mantenimiento.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="img/ascensor2.jpg" alt="Chania" width="500" height="245">
+        <div class="carousel-caption">
+          <h3>Mantenimiento</h3>
+          <p>Nos encargamos dle mantenimiento, aplicando planes consistentes, oprtunos y eficaces.</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="img/ascensor3.jpg" alt="Flower" width="500" height="245">
+        <div class="carousel-caption">
+          <h3>Urgencias</h3>
+          <p>Poseemos personal y equipo para atender y solucionar sus emergencias en tiempos acotados.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="img/ascensor4.jpg" alt="Flower" width="500" height="245">
+        <div class="carousel-caption">
+          <h3>Certificacion</h3>
+          <p>Poseemos toda la certificacion necesaria, con ello aseguramos nuestra calidad de servicio.</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+</div>
+<div class="col-sm-4">
+   <section >
+    
+    <div class="container">
+            <div class="row">
+                <div class="col-sm-3 " style="text-align:justify;" >
+                    <h1 class="titulo">Contacto</h1>
+                    <p class="parrafo" >
+ 
+          <!--<ul class="servicios">
+                  <li><span class="glyphicon glyphicon-ok"> Mecánica</span></li>
+                  <li><span class="glyphicon glyphicon-ok"> Hidráulica</span></li>
+                  <li><span class="glyphicon glyphicon-ok"> Niveladores de aire y potencia</span></li>
+                  <li><span class="glyphicon glyphicon-ok"> Tráfico de puertas de impacto</span></li>
+                  <li><span class="glyphicon glyphicon-ok"> Sistema de barreras de seguridad</span></li>
+                  <li><span class="glyphicon glyphicon-ok"> Niveladores hidráulicos</span></li>
+                  <li><span class="glyphicon glyphicon-ok"> Repuestos y accesorios</span></li>
+          </ul>-->
+</p>
+                </div>
+          
+            </div>
+        </div>
+   </section>
+
+</div>
+
+
+</div>
+  </section>
+  <section>   <div id="principal">
+
+    <div class="row">
+    <hr>
+     <div class="span8" id="divMain">
+
+
+    <h1 class="tit1">Contactenos</h1>
+                   <!--   <h3 style="color:#FF6633;"><?php echo $_GET[msg];?></h3> -->
+      				<td width="260" valign="top">
+
+					<br>
+
+					<br>
+
+					<h2>Hemos recibido su mensaje.</h2>
+
+					<br>
+
+					<br>
+
+					<h3>Pronto tomaremos contacto con usted.</h3>
+
+			
+
+					<?php 
+
+					$hoy = getdate();
+
+					$mensaje="Servilift<br><br>Contacto  Desde la WEB<br>";
+
+					$mensaje.="<br>Nombre : ".$_REQUEST["nombre"]." ".$_REQUEST["apellido"];
+
+					$mensaje.="<br>Email : ".$_REQUEST["correoelectronico"];
+
+					$mensaje.="<br>Categoria : ".$_REQUEST["categoria"];
+
+					$mensaje.="<hr><br><p>".$_REQUEST["message"]."</p><hr>";
+
+					$mensaje.="<br>Fecha del Mensaje : ";
+
+					$headers = "MIME-Version: 1.0\n"; 
+
+					$headers .= "Content-type: text/html; charset=iso-8859-1\n"; 
+
+					$headers .= "From: Contacto WEB Servilift <".$_REQUEST["correoelectronico"].">";
+
+					
+
+					
+
+					$dirigido = "contacto@servilift.cl";
+
+					mail($dirigido , "".$_REQUEST["categoria"], $mensaje, $headers);	
+					
+					
+					$dirigido = "juan.gutierrezcalquin@gmail.cl";
+
+					mail($dirigido , "".$_REQUEST["categoria"], $mensaje, $headers);					
+
+					?> 			
+
+
+
+			<section >
+
+  <div class="row contenido2">
+  <hr>
+  <div class="col-lg-4">
+              <div class="media-body">
+                            <h4 class="media-heading ">Enlaces Internos</h4>
+                            <hr>
+                            <p> 
+                                  <div class="col-md-12 col-sm-12">
+                                   
+                                      <div>
+                                          <ul class="arrow">
+                                              <li><a href="#">Empresa</a></li>
+                                              <li><a href="#">Equipo de Trabajo</a></li>
+                                              <li><a href="#">Clientes</a></li>
+                                              <li><a href="#">Servicios</a></li>
+                                              <li><a href="#">Preguntas Frecuentes</a></li>
+                                              <li><a href="contacto.html">Contacto</a></li>
+                                             
+                                          </ul>
+                                      </div>
+                                  </div>
+
+                            </p>
+                        </div>
+        </div>
+
+        <div class="col-lg-4">
+          <div class="media">
+                        <div class="pull-left">
+                            <i class="icon-hand-down icon-md"></i>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading ">Contactos</h4>
+                            <hr>
+                    
+                    <addess>
+                       <div style="display: inline-block;"><p><ol><abbr title="Telefono Fijo"><img  src="img/telefono.png"  class="img-responsive img-circle" alt=""></abbr>+562 28917816 </ol></p></div><br>
+                        <div style="display: inline-block;"><abbr title="Celular"><img  src="img/tmovil.jpg"   class="img-responsive img-circle"  alt=""></abbr> +569 78797072</div><br>
+                    </address>
+                  
+                
+                             </p>
+                        </div>
+                    </div>
+             
+       </div>
+        <div class="col-lg-4">       
+
+          <div class="media-body">
+                                    <h4 class="media-heading ">Enlaces Externos</h4>
+                                    <hr>
+                                    <p> 
+                                          <div class="col-md-12 col-sm-12">
+                                           
+                                              <div>
+                                                  <ul class="arrow">
+                                                      <li><a href="http://www.sec.cl/">SEC</a></li>
+                                                      <li><a href="http://www.minvu.cl/opensite_det_20150420182720.aspx">Normativa de Ascensores</a></li>
+                                                      <li><a href="http://www.sii.cl">SII</a></li>
+                                                      <li><a href="http://www.emol.com">EMOL</a></li>
+                                                      
+                                                     
+                                                  </ul>
+                                              </div>
+                                          </div>
+
+                                    </p>
+                                </div>
+ 
+
+   </div>
+   <hr>
+   </div>
+   <div class="row foot">
+    <div class="col-sm-8 pie center-block" style="vertical-align: middle;" >
+                    <a target="_blank" style="float:left; margin: 0 20px;" href="http://www.adegest.cl" title="Diseñado por Adegest"  Copyright </a> &copy; 2016 All Rights Reserved. Diseñado por Adegest Spa. www.adegest.cl  
+                   
+                    <img src="img/twitter.png" style="float:left;" width="23px" heigth="23px"/>  <img src="img/facebook.png" style="float:left;margin: 0 10px; " width="23px" heigth="23px"/> 
+                    
+
+                     </p>
+                </div> 
+                </div>
+  </section>
+  </body>
+  
+  </html>
